d9fe7ccb7335e791842876fa7ac3ee72dc6518ea
Alejandro Cruz <daniel_719021@hotmail.es>
2023-09-05 00:47:09 -0600

    nextui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import NavBar from './components/navbar/navbar.component';
+
+import { Card, CardBody, CardFooter, Image } from '@nextui-org/react';
+
+export default function App() {
+  const list = [
+    {
+      title: 'Orange',
+      img: '/images/fruit-1.jpeg',
+      price: '$5.50',
+    },
+    {
+      title: 'Tangerine',
+      img: '/images/fruit-2.jpeg',
+      price: '$3.00',
+    },
+    {
+      title: 'Raspberry',
+      img: '/images/fruit-3.jpeg',
+      price: '$10.00',
+    },
+    {
+      title: 'Lemon',
+      img: '/images/fruit-4.jpeg',
+      price: '$5.30',
+    },
+    {
+      title: 'Avocado',
+      img: '/images/fruit-5.jpeg',
+      price: '$15.70',
+    },
+    {
+      title: 'Lemon 2',
+      img: '/images/fruit-6.jpeg',
+      price: '$8.00',
+    },
+    {
+      title: 'Banana',
+      img: '/images/fruit-7.jpeg',
+      price: '$7.50',
+    },
+    {
+      title: 'Watermelon',
+      img: '/images/fruit-8.jpeg',
+      price: '$12.20',
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="">
+      <NavBar />
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-3 max-w-screen-md mx-auto">
+        {list.map((item, index) => (
+          <Card
+            shadow="sm"
+            key={index}
+            isPressable
+            className=""
+            onPress={() => console.log('item pressed')}
+          >
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={item.title}
+                className="w-full object-cover h-[140px]"
+                src={item.img}
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+              <b>{item.title}</b>
+              <p className="text-default-500">{item.price}</p>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
